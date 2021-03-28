c67d043fe3e5abe40a14a7c1ce0465701dde2fba
genesismandela <mandelasamboy@gmail.com>
2021-03-28 16:46:50 +0100

    Revert "Privacy"
    This reverts commit 25ce7546b5815fcb043ce60ff837dc341aa7f603.

--- main.js
+++ main.js
@@ -1,22 +1,5 @@
 // 'use strict';
 // =================== HOME PAGE ===================
-// Cookie setup
-
-window.cookieconsent.initialise({
-	palette: {
-		popup: {
-			background: "#343c66",
-			text: "#cfcfe8",
-		},
-		button: {
-			background: "#f71559",
-		},
-	},
-	content: {
-		href: "https://genesismandela.github.io/ibat/policy.html",
-	},
-});
-
 // HEADER MAIL ICON SHOW AND HIDE EMAIL
 const mailIcon = document.querySelector(".header-home__contact__mail-icon");
 mailIcon.addEventListener("click", function () {
